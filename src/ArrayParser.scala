81c2d1b4f87f170ac453dff18fb37fd5df5e44c3
emccallum <mccallum.ek@gmail.com>
2013-10-14 15:42:40 -0700

    Refactoring for readability

--- src/ArrayParser.scala
+++ src/ArrayParser.scala
@@ -1,29 +1,22 @@
 import java.io.PrintStream
 
-/**
- * Created with IntelliJ IDEA.
- * User: Thoughtworks
- * Date: 10/14/13
- * Time: 12:39 PM
- * To change this template use File | Settings | File Templates.
- */
 class ArrayParser {
   val printer = new PrintStream(System.out)
 
-  def findLongestString(arr: Array[String]): String = {
-    if (arr.length == 0) ""
+  def findLongestString(arrayOfStrings: Array[String]): String = {
+    if (arrayOfStrings.length == 0) ""
     else {
-      var currentLongestString = arr(0)
-      for (str <- arr)
+      var currentLongestString = arrayOfStrings(0)
+      for (str <- arrayOfStrings)
         if (str.length > currentLongestString.length)
           currentLongestString = str
       currentLongestString
     }
   }
 
-  def printOddIntegers(arr: Array[Int]) {
-    if (arr.length > 0)
-      for (num <- arr)
+  def printOddIntegers(arrayOfIntegers: Array[Int]) {
+    if (arrayOfIntegers.length > 0)
+      for (num <- arrayOfIntegers)
         if (num % 2 != 0)
           printer.println(num)
   }
--- test/ScalaAppTest.scala
+++ test/ScalaAppTest.scala
@@ -4,26 +4,26 @@ import org.specs2.mutable._
 import org.specs2.mock.Mockito
 
 class ArrayOfStringsTest extends Specification{
-  val ap = new ArrayParser()
+  val arrayParser = new ArrayParser()
 
   "An empty array" should {
     "return the empty string" in {
-      ap.findLongestString(Array()) must be("")
+      arrayParser.findLongestString(Array()) must be("")
     }
   }
   "An array with one string" should {
     "return that string" in {
-      ap.findLongestString(Array("hello")) must be("hello")
+      arrayParser.findLongestString(Array("hello")) must be("hello")
     }
   }
   "An array with two strings" should {
     "return the longest string" in {
-      ap.findLongestString(Array("hello", "you")) must be("hello")
+      arrayParser.findLongestString(Array("hello", "you")) must be("hello")
     }
   }
   "An array with two strings" should {
     "return the longest string regardless of order" in {
-      ap.findLongestString(Array("you", "hello")) must be("hello")
+      arrayParser.findLongestString(Array("you", "hello")) must be("hello")
     }
   }
 }
