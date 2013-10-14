1205231f7c2da53e5a473b608c1d7cc72c5b574b
emccallum <mccallum.ek@gmail.com>
2013-10-14 13:24:48 -0700

    New files

---
+++ src/ArrayParser.scala
@@ -0,0 +1,23 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Thoughtworks
+ * Date: 10/14/13
+ * Time: 12:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class ArrayParser {
+
+  def returnHelloWorld()
+  :String = "Hello, world!"
+
+  def findLongestString(arr: Array[String]) :String = {
+  if (arr.length == 0) ""
+  else {
+    val currentLongestString = arr(0)
+
+    ""
+  }
+
+  }
+
+}
